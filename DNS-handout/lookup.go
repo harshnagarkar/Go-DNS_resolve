45b17a374acd78a6d6f001ee5dd7b4f84915d4e3
Bryan Dixon <javawolfpack@gmail.com>
2020-10-19 08:58:38 -0700

    Revisions

--- DNS-handout/lookup.go
+++ DNS-handout/lookup.go
@@ -9,23 +9,29 @@ import (
 )
 
 func main() {
-
+	//Get the args without the executable
 	argsWithoutProg := os.Args[1:]
-
+	//get the number of args excluding the trailing output file
 	end := len(argsWithoutProg) - 1
+
+	//get the slices of the args for the inputs and outputs
 	inputs := argsWithoutProg[:end]
 	outfile := argsWithoutProg[end]
-	of, err := os.Create(outfile)
+
+	// For write access and to create the file if it doesn't exist
+	of, err := os.Create(outfile) 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+	defer of.Close()
 	for _, s := range inputs {
 		infile, err := os.Open(s) // For read access.
 		if err != nil {
 			log.Fatal(err)
 			return
 		}
+		defer if.Close()
 		scanner := bufio.NewScanner(infile)
 		for scanner.Scan() {
 			ips, err := net.LookupIP(scanner.Text())
