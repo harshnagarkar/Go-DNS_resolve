cb9cf9612dde22034511980558dfe05c50df8eff
Harsh Nagarkar <harshnagakar27@gmail.com>
2020-10-28 10:23:54 -0700

    working 2 model

--- DNS-handout/multi-lookup.go
+++ DNS-handout/multi-lookup.go
@@ -6,10 +6,17 @@ import (
 	"log"
 	"net"
 	"os"
+	"runtime"
 	"sync"
 )
 
-func request(name string, ch chan string) {
+type safe_channel struct {
+	mu      sync.Mutex
+	of_file *os.File
+	ch      chan string
+}
+
+func request(name string, ch chan string, wg *sync.WaitGroup) {
 
 	fmt.Println("test2")
 	infile, err := os.Open(name) // For read access.
@@ -19,15 +26,18 @@ func request(name string, ch chan string) {
 	}
 	defer infile.Close()
 	scanner := bufio.NewScanner(infile)
+
 	for scanner.Scan() {
 		var hostname = scanner.Text()
 		// resolve(filename)
 		fmt.Println(hostname)
+
 		ch <- hostname
 	}
+	wg.Done()
 }
 
-func resolve(filename string, ch chan string) {
+func resolve(c *safe_channel) {
 	// // fmt.Println("Results: "+filename)
 
 	// defer of.Close()
@@ -36,7 +46,7 @@ func resolve(filename string, ch chan string) {
 		// if(len(ch)>0){
 		// fmt.Println("hello")
 
-		var hostname = <-ch
+		var hostname = <-c.ch
 		// hostname<-ch
 		ips, err := net.LookupIP(hostname)
 
@@ -44,34 +54,46 @@ func resolve(filename string, ch chan string) {
 			fmt.Fprintf(os.Stderr, "dnslookup error: %s\n", hostname)
 		}
 
-		of, err := os.Open(filename) // For read access.
-		if err != nil {
-			log.Fatal(err)
-			return
-		}
+		// of, err := os.(filename) // For read access.
+		// if err != nil {
+		// 	log.Fatal(err)
+		// 	return
+		// }
 
+		//Go example 9
+		// struct element file pointer and mutex
+		// net.
+		c.mu.Lock()
 		if len(ips) > 0 {
 			fmt.Println(hostname + " resolve: " + ips[0].String())
 			//pass to the channel
-			fmt.Fprintf(of, "%s %s\n", hostname, ips[0].String())
+			data := hostname
+			// data+=hostname
+			for i := 0; i < len(ips); i++ {
+				data = data + " " + ips[i].String()
+			}
+			data += "\n"
+			// fmt.Fprintf(c.of_file, "%s %s\n", hostname, ips[0].String())
+			fmt.Fprint(c.of_file, data)
 		} else {
-			fmt.Fprintf(of, "%s \n", hostname)
+			fmt.Fprintf(c.of_file, "%s \n", hostname)
 			//pass to the channel
 		}
-		of.Close()
+		c.mu.Unlock()
+		// of.Close()
 		// }
 	}
 }
 
 func main() {
-	ch := make(chan string)
+	ch := make(chan string, 5)
 	//Get the args without the executable
 	argsWithoutProg := os.Args[1:]
 	//get the number of args excluding the trailing output file
 	end := len(argsWithoutProg) - 1
 
 	//get the slices of the args for the inputs and outputs
-	// inputs := argsWithoutProg[:end]
+	inputs := argsWithoutProg[:end]
 	outfile := argsWithoutProg[end]
 
 	// For write access and to create the file if it doesn't exist
@@ -81,16 +103,24 @@ func main() {
 		fmt.Println(err)
 		return
 	}
-	of.Close()
+	// of.Close()
 
 	var wg sync.WaitGroup
-	wg.Add(2)
+	safe_ch := safe_channel{of_file: of, ch: ch}
+
 	fmt.Println("test")
-	go request("input/names1.txt", ch)
-	go resolve(outfile, ch)
+
+	wg.Add(len(inputs) + 1)
+	for _, s := range inputs {
+		// ch <- s
+		fmt.Println(s)
+		go request(s, ch, &wg)
+	}
+	fmt.Printf("The number of cpu are %d", runtime.NumCPU())
+	wg.Add(runtime.NumCPU())
+	for i := 0; i < runtime.NumCPU(); i++ {
+		go resolve(&safe_ch)
+	}
 	wg.Wait()
-	// for _, s := range inputs {
-	// 	ch <- s
-	// }
 
 }
